eae0979665f8d7a90a3e1fe43398e0737fa5ea85
Somoy khan <k.somoykhan@gmail.com>
2020-05-28 08:08:27 +0600

    fix: updated prod api url

--- src/app/all-services.service.ts
+++ src/app/all-services.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AllServicesService {
+  api: any = environment.api
   constructor (private httpClient: HttpClient) {}
   httpOptions = {
     headers: new HttpHeaders({
@@ -15,14 +16,14 @@ export class AllServicesService {
 
   getLabels (): Observable<any> {
     return this.httpClient.get<any>(
-      'http://localhost:3000/labels',
+      `${this.api}labels`,
       this.httpOptions
     )
   }
 
   postTree (data: any): Observable<any> {
     return this.httpClient.post<any>(
-      'http://localhost:3000/result',
+      `${this.api}result`,
       data,
       this.httpOptions
     )
